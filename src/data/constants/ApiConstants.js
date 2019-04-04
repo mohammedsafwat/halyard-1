7f2d187b370357db6dc1a706d6b5f901e5285b65
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-04 08:31:20 +0200

    Create a new object 'ApiConstants' with all needed properties for booking's Api Queries.

---
+++ src/data/constants/ApiConstants.js
@@ -0,0 +1,8 @@
+export const ApiConstants = {
+    authentication: {
+        username: 'team_abdullatif',
+        password: 'NUFURrv6r3fjfRu'
+    },
+    baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
+    hotelsEndpoint: 'hotels'
+}

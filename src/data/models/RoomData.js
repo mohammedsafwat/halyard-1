599a4ccf13dae046911678293335d8a6359ee3a4
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-05 08:37:14 +0200

    feat: Add a new model class for 'RoomData'.

---
+++ src/data/models/RoomData.js
@@ -0,0 +1,9 @@
+export default class RoomData {
+    constructor(roomName, roomId, roomPhotos, roomDescription, roomInfo) {
+        this.roomName = roomName;
+        this.roomId = roomId;
+        this.roomPhotos = roomPhotos;
+        this.roomDescription = roomDescription;
+        this.roomInfo = roomInfo;
+    }
+}

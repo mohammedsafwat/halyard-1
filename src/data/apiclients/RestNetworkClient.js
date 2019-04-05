4bca46a30383b09b0af3a82e68db6232303b5cc0
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-05 08:39:57 +0200

    refactor: Rename 'result' parameter to 'response'.

--- src/data/apiclients/RestNetworkClient.js
+++ src/data/apiclients/RestNetworkClient.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import RequestType from '../helpers/RequestType'
 
 export const RestNetworkClient = implement(RestNetworkClientProtocol)({
-    async performRequest(requestUrl, requestType, params) {
-        const result = await requestType == RequestType.get ?
-                        axios.get(requestUrl, { params }) : axios.post(requestUrl, params);
-        return result;
+    async performRequest(requestUrl, requestType, parameters) {
+        const response = await requestType == RequestType.get ?
+                        axios.get(requestUrl, { parameters }) : axios.post(requestUrl, parameters);
+        return response;
     }
 });

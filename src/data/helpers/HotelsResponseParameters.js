64b3b15aab2f89d5c28e79057cfb356867d5816c
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-05 08:40:27 +0200

    Create a new object holding the hotels response keys.

---
+++ src/data/helpers/HotelsResponseParameters.js
@@ -0,0 +1,29 @@
+export const HotelsResponseParameters = {
+    hotelId: "hotel_id",
+    hotelData: "hotel_data",
+    roomData: "room_data",
+    hotelDataParameters: {
+        numberOfRooms: "number_of_rooms",
+        location: "location",
+        isClosed: "is_closed",
+        class: "class",
+        hotelDescription: "hotel_description",
+        reviewScore: "review_score",
+        cityId: "city_id",
+        hotelPhotos: "hotel_photos",
+        numberOfReviews: "number_of_reviews",
+        ranking,
+        url,
+        city,
+        country,
+        address,
+        name
+    },
+    roomDataParameters: {
+        roomName: "room_name",
+        roomId: "room_id",
+        roomPhotos: "room_photos",
+        roomDesription: "room_description",
+        roomInfo: "room_info"
+    }
+}

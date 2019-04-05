5a210b7763b256b4022f720891f98417015d613f
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 00:51:56 +0200

    Add a new url for a temporary 'corsProxyUrl'. Use 'ApiConstants' as the default export.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -1,8 +1,11 @@
-export const ApiConstants = {
+const ApiConstants = {
     authentication: {
         username: 'team_abdullatif',
         password: 'NUFURrv6r3fjfRu'
     },
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
+    corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
     hotelsEndpoint: 'hotels'
 }
+
+export default ApiConstants;

3a2ccb4c094fbadfd7ded2867a128655ec0cc9b4
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-05 08:37:27 +0200

    feat: Add a new model class for 'Photo'.

---
+++ src/data/models/Photo.js
@@ -0,0 +1,7 @@
+export default class Photo {
+    constructor(urlOriginal, urlMax300, urlSquare60) {
+        this.urlOriginal = urlOriginal;
+        this.urlMax300 = urlMax300;
+        this.urlSquare60 = urlSquare60;
+    }
+}

d6d43ef82628e09874c3d1ded98054427f3fa472
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 00:47:43 +0200

    Import 'Interface' and 'type' from 'implement-js'. Use 'RestNetworkClientProtocol' as the default export.

--- src/data/protocols/RestNetworkClientProtocol.js
+++ src/data/protocols/RestNetworkClientProtocol.js
@@ -1,6 +1,10 @@
+import { Interface, type } from 'implement-js'
+
 const RestNetworkClientProtocol = Interface('RestNetworkClientProtocol')({
     performRequest: type('function')
 }, {
     error: true,
     strict: true
-})
+});
+
+export default RestNetworkClientProtocol;

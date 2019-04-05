dfcaa9b0d8ef9f90c727ae367eb7d441f557d842
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-05 08:38:33 +0200

    feat: Create a new model class 'Hotel' holding the main three properties: 'hotelId', 'hotelData' and 'roomData'.

---
+++ src/data/models/Hotel.js
@@ -0,0 +1,7 @@
+export default class Hotel {
+    constructor(hotelId, hotelData, roomData) {
+        this.hotelId = hotelId;
+        this.hotelData = hotelData;
+        this.roomData = roomData;
+    }
+}

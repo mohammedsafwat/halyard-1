0299c3709356bcf6afa14297bafa1f5970ce3e99
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 00:48:06 +0200

    Import 'Interface' and 'type' from 'implement-js'. Use 'HotelsDataSource' as the default export.

--- src/data/protocols/HotelsDataSource.js
+++ src/data/protocols/HotelsDataSource.js
@@ -1,6 +1,10 @@
+import { Interface, type } from 'implement-js'
+
 const HotelsDataSource = Interface('HotelsDataSource')({
     hotels: type('function')
 }, {
     error: true,
     strict: true
-})
+});
+
+export default HotelsDataSource;

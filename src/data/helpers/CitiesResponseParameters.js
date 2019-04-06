5cebc320f206dcae4dd93a82a4af3f987a7a158f
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 19:09:35 +0200

    Create a new 'CitiesResponseParameters' object holding all string values for cities response parameters.

---
+++ src/data/helpers/CitiesResponseParameters.js
@@ -0,0 +1,9 @@
+const CitiesResponseParameters = {
+    country: "country",
+    cityId: "city_id",
+    location: "location",
+    numberOfHotels: "nr_hotels",
+    name: "name"
+};
+
+export default CitiesResponseParameters;

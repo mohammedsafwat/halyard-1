450abdcb095055989f7ef6a1357074ae73ab96b2
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 19:10:13 +0200

    Add a new endpoint 'cities' to 'ApiConstants'.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -5,7 +5,8 @@ const ApiConstants = {
     },
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
     corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
-    hotelsEndpoint: 'hotels'
+    hotelsEndpoint: 'hotels',
+    citiesEndpoint: 'cities'
 }
 
 export default ApiConstants;

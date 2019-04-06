e5cd24cffce19a0861edcee73aa122d82c5ad7c3
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-06 19:08:07 +0200

    Create a new 'City' model class.

---
+++ src/data/models/City.js
@@ -0,0 +1,9 @@
+export default class City {
+    constructor(country, cityId, location, numberOfHotels, name) {
+        this.country = country;
+        this.cityId = cityId;
+        this.location = location;
+        this.numberOfHotels = numberOfHotels;
+        this.name = name;
+    }
+}

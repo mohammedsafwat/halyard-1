0a45befff2c904b681e75ff49fd9866119258db5
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-07 12:42:47 +0200

    feat: Create UserLocationRemoteDataSource'.

---
+++ src/data/datasources/remote/UserLocationRemoteDataSource.js
@@ -0,0 +1,51 @@
+import implement from 'implement-js'
+import UserLocationDataSource from '../../protocols/UserLocationDataSource'
+import geolocator from 'geolocator'
+import ApiConstants from '../../constants/ApiConstants'
+import LocationResponseParameters from '../../helpers/LocationResponseParameters'
+import Location from '../../models/Location'
+
+const UserLocationRemoteDataSource = implement(UserLocationDataSource)({
+    async userLocation() {
+        geolocator.config({
+            language: "en",
+            google: {
+                version: "3",
+                key: ApiConstants.googleApiKey
+            }
+        });
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumWait: 10000,     // max wait time for desired accuracy
+            maximumAge: 0,          // disable cache
+            desiredAccuracy: 30,    // meters
+            fallbackToIP: true,     // fallback to IP if Geolocation fails or rejected
+            addressLookup: true,    // requires Google API key if true
+            timezone: true         // requires Google API key if true
+        };
+        
+        return new Promise((resolve, reject) => {
+            geolocator.locate(options, (error, location) => {
+                if (error) reject(error);
+                resolve(parseLocation(location));
+            });
+        }).catch(handleError);
+
+    }
+});
+
+const parseLocation = locationData => {
+    const coordinates = locationData[LocationResponseParameters.coords];
+    const latitude = coordinates[LocationResponseParameters.latitude];
+    const longitude = coordinates[LocationResponseParameters.longitude];
+    const location = new Location(latitude, longitude);
+    return location
+}
+
+const handleError = error => {
+    console.warn(error);
+    return null;
+};
+
+export default UserLocationRemoteDataSource;

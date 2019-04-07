54efb5990839035d306a0d85abe41a44d0db3c1b
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-07 18:29:40 +0200

    refactor: Rename 'hotelsAvailability' to 'hotelsAvailabilityEndpoint'.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -6,7 +6,7 @@ const ApiConstants = {
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
     corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
     hotelsEndpoint: 'hotels',
-    hotelsAvailability: 'hotelAvailability',
+    hotelsAvailabilityEndpoint: 'hotelAvailability',
     citiesEndpoint: 'cities',
     googleApiKey: 'AIzaSyA2jceusIM_xewnQgj0LS0CzKyNGxb8EVA'
 }

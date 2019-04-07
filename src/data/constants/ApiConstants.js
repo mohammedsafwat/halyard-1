13288bbf483db5feb333711f91b0207406529b0c
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-07 12:43:50 +0200

    Add a new 'hotelsAvailability' endpoint string to 'ApiConstants'.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -6,7 +6,9 @@ const ApiConstants = {
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
     corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
     hotelsEndpoint: 'hotels',
-    citiesEndpoint: 'cities'
+    hotelsAvailability: 'hotelAvailability',
+    citiesEndpoint: 'cities',
+    googleApiKey: 'AIzaSyA2jceusIM_xewnQgj0LS0CzKyNGxb8EVA'
 }
 
 export default ApiConstants;

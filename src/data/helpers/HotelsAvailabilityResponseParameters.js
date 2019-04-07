22309af2b2c3eb645abf4fce522b1077c4279a7c
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-07 12:43:13 +0200

    Create a new object 'HotelsAvailabilityResponseParameters' object holding all response strings.

---
+++ src/data/helpers/HotelsAvailabilityResponseParameters.js
@@ -0,0 +1,15 @@
+const HotelsAvailabilityResponseParameters = {
+    reviewScoreWord: "review_score_word",
+    hotelId: "hotel_id",
+    stars: "stars",
+    price: "price",
+    address: "address",
+    reviewScore: "review_score",
+    hotelName: "hotel_name",
+    postCode: "postcode",
+    country: "country",
+    hotelCurrencyCode: "hotel_currency_code",
+    reviewNumber: "review_nr"
+};
+
+export default HotelsAvailabilityResponseParameters;

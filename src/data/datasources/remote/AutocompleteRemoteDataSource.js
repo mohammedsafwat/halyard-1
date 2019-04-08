e6a0d29cf56b1d30413332ea7016ead5b12deb6e
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-08 09:28:33 +0200

    Create a new object 'AutocompleteResponseParameters' holding all response parameters keys.

---
+++ src/data/datasources/remote/AutocompleteRemoteDataSource.js
@@ -0,0 +1,7 @@
+import implement from 'implement-js'
+import RestNetworkClient from '../../apiclients/RestNetworkClient'
+import AutocompleteDataSource from '../../protocols/AutocompleteDataSource'
+import ApiConstants from '../../constants/ApiConstants'
+import RequestType from '../../helpers/RequestType'
+import AutocompleteResponseParameters from '../../helpers/AutocompleteResponseParameters'
+
---
+++ src/data/helpers/AutocompleteResponseParameters.js
@@ -0,0 +1,19 @@
+const AutocompleteResponseParameters = {
+    country: "country",
+    cityUfi: "city_ufi",
+    numberOfHotels: "nr_hotels",
+    longitude: "longitude",
+    latitude: "latitude",
+    name: "name",
+    language: "language",
+    url: "url",
+    type: "type",
+    timezone: "timezone",
+    id: "id",
+    label: "label",
+    countryName: "country_name",
+    region: "region",
+    cityName: "city_name"
+};
+
+export default AutocompleteResponseParameters;

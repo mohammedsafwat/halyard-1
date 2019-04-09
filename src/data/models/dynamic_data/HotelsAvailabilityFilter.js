8c8049a228fc084459c301f371104c9f725fef5d
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-09 04:58:01 +0200

    refactor: Change 'cityIds' parameter to 'latitude' and 'longitude'.

--- src/data/models/dynamic_data/HotelsAvailabilityFilter.js
+++ src/data/models/dynamic_data/HotelsAvailabilityFilter.js
@@ -1,8 +1,11 @@
 export default class HotelsAvailabilityFilter {
-    constructor(checkIn, checkOut, cityIds, room1) {
+    constructor(checkIn, checkOut, latitude, longitude, room1, offset, rows) {
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        this.cityIds = cityIds;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.room1 = room1;
+        this.offset = offset;
+        this.rows = rows;
     }
 }

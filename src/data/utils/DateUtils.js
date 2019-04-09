a53862192ca7ae223a206d83bd6e094afdb7bc65
Mohammed Safwat <mail.msafwat@gmail.com>
2019-04-09 04:57:31 +0200

    Create a new 'DateUtils' implementation for 'DateUtilsProtocol'.

---
+++ src/data/utils/DateUtils.js
@@ -0,0 +1,15 @@
+import implement from 'implement-js'
+import dateFormat from 'dateformat'
+import DateUtilsProtocol from '../protocols/DateUtilsProtocol'
+
+const DateUtils = implement(DateUtilsProtocol)({
+    now: new Date(),
+    nowDate() {
+        return dateFormat(this.now, "yyyy-mm-dd");
+    },
+    nextMonthsDate(numberOfMonth) {
+        return dateFormat(new Date(this.now.getFullYear(), this.now.getMonth() + numberOfMonth, 1), "yyyy-mm-dd");
+    }
+});
+
+export default DateUtils;

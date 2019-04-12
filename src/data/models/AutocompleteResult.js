5046d18fda501635c088def166b81d3358cf74c6
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-12 13:28:28 +0200

    Create a new model 'AutocompleteResult' with four properties 'cityName', 'countryName', 'latitude' and 'longitude'.

---
+++ src/data/models/AutocompleteResult.js
@@ -0,0 +1,8 @@
+export default class AutocompleteResult {
+    constructor(cityName, countryName, latitude, longitude) {
+        this.cityName = cityName;
+        this.countryName = countryName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}

1a4ba3da5334a8db1b7e803b857d16efd7f3149a
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-12 13:29:08 +0200

    Update 'App.js' to include some examples for Autocomplete and fetching hotels of a random city.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import HotelsRemoteDataSource from './data/datasources/remote/HotelsRemoteDataSource'
-import UserLocationRemoteDataSource from './data/datasources/remote/UserLocationRemoteDataSource'
-import HotelsAvailabilityRemoteDataSource from './data/datasources/remote/HotelsAvailabilityRemoteDataSource'
-import HotelsFilter from './data/models/static_data/HotelsFilter'
-import HotelsAvailabilityFilter from './data/models/dynamic_data/HotelsAvailabilityFilter'
-import DateUtils from './data/utils/DateUtils'
+import LocationRandomizerFilter from '../src/data/models/LocationRandomizerFilter'
+import LocationRandomizerRemoteDataSource from '../src/data/datasources/remote/LocationRandomizerRemoteDataSource'
+import AutocompleteRemoteDataSource from '../src/data/datasources/remote/AutocompleteRemoteDataSource'
+import HotelsRepository from '../src/data/datasources/repositories/HotelsRepository'
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.scss'
@@ -16,42 +14,28 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.fetchUserLocation()
-      .then(userLocation => {
-        this.fetchHotelsData(userLocation)
-        console.log(userLocation)
-      }).catch(error => {
-        console.warn(error);
-      });
+    // Autocomplete Example.
+    this.autocomplete('Ber')
+    .then(results => console.log(results));
+
+    // Fetch Random Location Example.
+    this.fetchRandomCity()
+        .then(city => { 
+          // Pass in the city, the starting offset and the number of rows to fetch.
+          // The result is an array of 'HotelCardData' objects.
+          HotelsRepository.hotels(city, 0, 20)
+            .then(result => console.log(result));
+        })
+        .catch(error => console.warn(error));
   }
 
-  fetchHotelsData = async (userLocation) => {
-    const hotelsAvailabilityFilter = new HotelsAvailabilityFilter(DateUtils.nowDate(), DateUtils.nextMonthsDate(1), userLocation.latitude, userLocation.longitude, 'A,A', 0, 20);
-    this.fetchHotelsAvailability(hotelsAvailabilityFilter)
-      .then(hotelsAvailabilityData => {
-        this.setState(() => ({
-          hotelsAvailabilityData
-        }));
-        const hotelIds = hotelsAvailabilityData.map(hotelAvailability => {
-          return hotelAvailability.hotelId;
-        }).join(',');
-        const hotelsFilter = new HotelsFilter(hotelIds, 0, 20);
-        this.fetchHotels(hotelsFilter).then(hotelsData => this.setState(() => ({
-          hotelsData
-        })));
-      });
-  };
-
-  fetchUserLocation = async () => {
-    return await UserLocationRemoteDataSource.userLocation();
+  autocomplete = async(text) => {
+    return await AutocompleteRemoteDataSource.autocomplete(text);
   }
 
-  fetchHotels = async (hotelsFilter) => {
-    return await HotelsRemoteDataSource.hotels(hotelsFilter);
-  }
-
-  fetchHotelsAvailability = async (hotelsAvailabilityFilter) => {
-    return await HotelsAvailabilityRemoteDataSource.hotelsAvailability(hotelsAvailabilityFilter);
+  fetchRandomCity = async () => {
+    const locationRandomizerFilter = new LocationRandomizerFilter(10);
+    return await LocationRandomizerRemoteDataSource.randomLocation(locationRandomizerFilter);
   }
   
   render() {

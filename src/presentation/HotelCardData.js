20eacf27b533c445d0c8b832f0311d4d5a6babd0
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-12 13:26:09 +0200

    Add new model 'HotelCardData'.

---
+++ src/presentation/HotelCardData.js
@@ -0,0 +1,11 @@
+export default class HotelCardData {
+    constructor(hotelId, hotelImage, hotelScore, hotelPrice, hotelUrl, hotelImageAlt, hotelCurrencyCode) {
+        this.hotelId = hotelId;
+        this.hotelImage = hotelImage;
+        this.hotelScore = hotelScore;
+        this.hotelPrice = hotelPrice;
+        this.hotelUrl = hotelUrl;
+        this.hotelImageAlt = hotelImageAlt;
+        this.hotelCurrencyCode = hotelCurrencyCode;
+    }
+}

c431b9bae37de83d298ba3672f34a4d23dca5c15
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-12 13:31:19 +0200

    Add a new endpoint 'autocomplete' and update the authentication object for now to use the hard-coded values.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -1,10 +1,11 @@
 const ApiConstants = {
-    authentication: process.env.REACT_APP_BOOKING_AUTH_CREDENCIALS,
+    authentication: { username: 'team_abdullatif', password: 'NUFURrv6r3fjfRu' },
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
     corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
     hotelsEndpoint: 'hotels',
     hotelsAvailabilityEndpoint: 'hotelAvailability',
     citiesEndpoint: 'cities',
+    autocompleteEndpoint: 'autocomplete',
     googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY
 }
 

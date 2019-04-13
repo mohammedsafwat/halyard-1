776764cdb8266e94213237142fa3c2e2258f15fc
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-14 00:14:58 +0200

    Implement infinite scrolling.

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import Card from 'components/Card'
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export default ({ cards, hasError }) => (
-      <>
-        <div className="card-container">
-        { hasError ?
-          <div>There's no suggestions over here. Would you like to try <button onClick="">another random destination?</button></div>
-          :
-          cards.map(card => (
-            <Card key={card.id} image={card.image} score={card.score} price={card.price}/>
-          ))
-        }
-        </div>
-      </>
+export default ({ cards, fetchMoreHotels, hasError }) => (
+  <>
+    { hasError ?
+      <div>There's no suggestions over here. Would you like to try <button onClick="">another random destination?</button></div>
+      :
+      <InfiniteScroll
+        className="card-container"
+        dataLength={cards.length}
+        next={fetchMoreHotels}
+        hasMore={true}
+        loader={<div></div>}
+        scrollThreshold={0.4}
+        height={400}
+      >
+      { cards.map(card => (
+        <Card key={card.hotelId} image={card.hotelImage} score={card.hotelScore} price={card.hotelPrice}/>
+      )) }
+      </InfiniteScroll>
+    }
+  </>
 )

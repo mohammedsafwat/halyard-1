cf6d4a38eb79e81c5185c718d3f06ae218362e5d
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-04-13 06:17:40 +0200

    Create a new authentication object inside 'ApiConstants.js'.

--- src/data/constants/ApiConstants.js
+++ src/data/constants/ApiConstants.js
@@ -1,5 +1,5 @@
 const ApiConstants = {
-    authentication: { username: 'team_abdullatif', password: 'NUFURrv6r3fjfRu' },
+    authentication: { username: process.env.REACT_APP_BOOKING_AUTH_USERNAME, password: process.env.REACT_APP_BOOKING_AUTH_PASSWORD },
     baseUrl: 'https://distribution-xml.booking.com/2.4/json/',
     corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
     hotelsEndpoint: 'hotels',
